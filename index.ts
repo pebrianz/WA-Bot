838ebed6c0c87b89392ee189a47fd207c21dc873
pebrianz <pebrianz117@gmail.com>
2023-01-20 13:48:50 +0700

    make it better

--- index.ts
+++ index.ts
@@ -1,3 +1,6 @@
+import path from "path";
+import { readdirSync } from "fs";
+
 import makeWASocket, {
   fetchLatestBaileysVersion,
   MessageRetryMap,
@@ -8,35 +11,36 @@ import makeWASocket, {
 import { Boom } from "@hapi/boom";
 import pino from "pino";
 
-import { readdirSync } from "fs";
-import path, { basename } from "path";
-
-import Message from "./utils/message";
 import MAIN_LOGGER from "./utils/logger";
+import Message from "./utils/message";
 
-(async () => {
+const getListLibs = async () => {
   const files = readdirSync(path.join(__dirname, "/libs/")).filter((file) =>
     file.endsWith(".ts")
   );
-  console.log(files);
-  console.log(basename(__filename));
-})();
+  return files;
+};
 
 const useStore = !process.argv.includes("--no-store");
 const doReplies = !process.argv.includes("--no-reply");
 
 const logger = MAIN_LOGGER.child({});
 logger.level = "silent";
+
+// external map to store retry counts of messages when decryption/encryption fails
+// keep this out of the socket itself, so as to prevent a message decryption/encryption loop across socket restarts
 const msgRetryCounterMap: MessageRetryMap = {};
 
+// the store maintains the data of the WA connection in memory
+// can be written out to a file & read from it
 const store = useStore ? makeInMemoryStore({ logger }) : undefined;
 store?.readFromFile("./tohka_yatogami_store_multi.json");
-
+// save every 10s
 setInterval(() => {
   store?.writeToFile("./tohka_yatogami_store_multi.json");
 }, 10_000);
 
-async function connectToWhatsApp() {
+async function startSock() {
   const { version, isLatest } = await fetchLatestBaileysVersion();
   console.log(`using WA v${version.join(".")}, isLatest: ${isLatest}`);
 
@@ -46,7 +50,7 @@ async function connectToWhatsApp() {
     logger: pino({ timestamp: () => `,"time":"${new Date().toJSON()}"` }),
   });
 
-  const client = makeWASocket({
+  const sock = makeWASocket({
     version,
     logger,
     printQRInTerminal: true,
@@ -64,49 +68,39 @@ async function connectToWhatsApp() {
     },
   });
 
-  store.bind(client.ev);
+  store.bind(sock.ev);
 
-  client.ev.on("creds.update", saveState);
+  sock.ev.on("creds.update", saveState);
 
-  client.ev.on("connection.update", (update) => {
+  sock.ev.on("connection.update", (update) => {
     const { connection, lastDisconnect } = update;
     if (connection === "close") {
-      if (!lastDisconnect) return;
-      const reason = (lastDisconnect.error as Boom)?.output?.statusCode;
-      console.log(
-        "connection closed due to ",
-        lastDisconnect.error,
-        ", reconnecting ",
-        reason
-      );
-      if (reason === DisconnectReason.connectionClosed) {
-        connectToWhatsApp();
-      }
-      if (reason === DisconnectReason.connectionLost) {
-        connectToWhatsApp();
-      }
-      if (reason === DisconnectReason.timedOut) {
-        connectToWhatsApp();
+      // reconnect if not logged out
+      if (
+        (lastDisconnect?.error as Boom)?.output?.statusCode !==
+        DisconnectReason.loggedOut
+      ) {
+        startSock();
+      } else {
+        console.log("Connection closed. You are logged out.");
       }
     } else if (connection === "open") {
       console.log("opened connection");
     }
   });
 
-  client.ev.on("messages.upsert", async ({ messages }) => {
+  sock.ev.on("messages.upsert", async ({ messages }) => {
     try {
       const message = messages.at(0);
       if (!message) return;
-      console.log(message);
+      //      console.log(message);
       const msg = new Message(message);
-      if (msg.body == "test") {
-        client.sendMessage(msg.jid, { text: "helo" });
-      }
+      console.log(msg);
     } catch (e: any) {
       console.error(e.messages);
     }
   });
-  return client;
+  return sock;
 }
 
-connectToWhatsApp();
+startSock();
